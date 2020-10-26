e91e8c9b32d0ea6598a31ab4cd608c6904ed78bc
Akash Damle <akash.damle@outlook.com>
2020-10-26 16:47:23 +0530

    Added Shop Forms

---
+++ apps/shop/forms.py
@@ -0,0 +1,58 @@
+from django import forms
+from .models import Tag, Category, SubCategory, Product
+from .models import Order, OrderItem, UserReview, Listing
+from .models import Payment
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class CaetgoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class SubCategoryForm(forms.ModelForm):
+    class Meta:
+        model = SubCategory
+        fields = '__all__'
+
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = '__all__'
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderItemForm(forms.ModelForm):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class UserReviewForm(forms.ModelForm):
+    class Meta:
+        model = UserReview
+        fields = '__all__'
+
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = '__all__'
