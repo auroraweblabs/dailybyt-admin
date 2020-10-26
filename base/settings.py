c0f46a51f43fd1717b17ff4926ec77b56fdb7504
Akash Damle <akash.damle@outlook.com>
2020-10-26 13:15:32 +0530

    PWA Setup done

--- base/settings.py
+++ base/settings.py
@@ -9,7 +9,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [
     BASE_DIR/'mystatic', ]
 
-SECRET_KEY = 'jchqogoyf0813yf1h3f1h3f1y08fy10313p'
+SECRET_KEY = 'h4=g!0fxfud=wi@29kbxxgku_m@o@=6@x%432+1l8bd_n-h@&b'
 
 DEBUG = True
 
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'knox',
     'rest_framework',
     'pwa',
 ]
@@ -62,7 +64,7 @@ WSGI_APPLICATION = 'base.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django',
+        'NAME': 'ecommerce',
         'USER': 'postgres',
         'PASSWORD': 'psql',
         'HOST': '127.0.0.1',
@@ -104,3 +106,38 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
+
+PWA_APP_NAME = 'eCommerce'
+PWA_APP_DESCRIPTION = "It's time to do buisness online."
+PWA_APP_THEME_COLOR = '#348537'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/images/logo.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/images/logo.png',
+        'sizes': '160x160'
+    }
+]
+PWA_X = '(device-width: 320px) and (device-height: 568px)'
+PWA_Y = 'and (-webkit-device-pixel-ratio: 2)'
+
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/images/logo.png',
+        'media': PWA_X + PWA_Y
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
